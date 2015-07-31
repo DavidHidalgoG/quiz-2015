fb52bea80a84ae5d9de7f39c640ace44da10af57
David Hidalgo González <d.hidalgogonzalez@gmail.com>
2015-07-31 11:13:25 +0200

    Temas de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,63 +46,54 @@ exports.answer = function(req, res) {
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
-// GET /quizes/new
-exports.new = function(req, res) {
-  var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
-  );
 
-  res.render('quizes/new', {quiz: quiz, errors: []});
+exports.new = function(req,res){
+	var quiz = models.Quiz.build({pregunta:"Pregunta",respuesta:"Respuesta"});
+
+	res.render('quizes/new',{temas: models.Quiz.rawAttributes.tema.values, quiz:quiz,errors:[]});
 };
 
-// POST /quizes/create
-exports.create = function(req, res) {
-  var quiz = models.Quiz.build( req.body.quiz );
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
 
-  quiz.validate().then(function(err){
-      if (err) {
-        res.render('quizes/new', {quiz: quiz, errors: err.errors});
-      } else {
-        quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes')}) 
-      }      // res.redirect: Redirección HTTP a lista de preguntas
-  }
-  );
-}; 
-
-exports.edit = function(req, res) {
-  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
-
-  res.render('quizes/edit', {quiz: quiz, errors: []});
+	quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new',{quiz:quiz,errors:err.errors});
+		}else{
+			quiz.save({fields:["pregunta","respuesta","tema"]}).then(function(){
+				res.redirect('/quizes');
+			})
+		}
+	});
 };
 
+exports.edit = function(req,res){
+	var quiz = req.quiz;
 
-// PUT /quizes/:id
-exports.update = function(req, res) {
-  req.quiz.pregunta  = req.body.quiz.pregunta;
-  req.quiz.respuesta = req.body.quiz.respuesta;
-
-  req.quiz
-  .validate()
-  .then(
-    function(err){
-      if (err) {
-        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
-      } else {
-        req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes');});
-      }     // Redirección HTTP a lista de preguntas (URL relativo)
-    }
-  ).catch(function(error){next(error)});
+	res.render('quizes/edit',{temas: models.Quiz.rawAttributes.tema.values, quiz:quiz,errors:[]});
 };
 
-// DELETE /quizes/:id
-exports.destroy = function(req, res) {
-  req.quiz.destroy().then( function() {
-    res.redirect('/quizes');
-  }).catch(function(error){next(error)});
+exports.update = function(req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
+
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit',{quiz:req.quiz,errors:err.errors});
+			}else{
+				req.quiz.save({fields:["pregunta","respuesta","tema"]})
+				.then(function(){res.redirect('/quizes');});
+			}
+		}
+		);
+};
+
+exports.destroy = function(req,res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
 };
 
 // GET /creditos
--- models/quiz.js
+++ models/quiz.js
@@ -1,16 +1,19 @@
-// Definicion del modelo de Quiz
-
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-  	'Quiz',
-    { pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
-      respuesta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      }
+module.exports = function(sequelize, DataTypes)
+{
+  return sequelize.define('Quiz',
+  {
+    pregunta: {
+      type: DataTypes.STRING,
+      validate:{ notEmpty: {msg: "-> Falta Pregunta"}}
+  },
+    respuesta: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "-> Falta respuesta"}}
+  },
+    tema: {
+      type: DataTypes.ENUM,
+      values: ['Otro','Humanidades','Ocio','Ciencias','Tecnologia','Geografia']
     }
-  );
-} 
+
+  });
+}
